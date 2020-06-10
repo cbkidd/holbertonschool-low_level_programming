c2fab1427c2621a3caeae46bb63b40d6a0d433c3
cbkidd <ckiddgodsea@gmail.com>
2020-06-10 23:43:20 +0000

    fix repository

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+* main - entry point
+*
+* puts - put string
+*
+* Return - write string
+*
+*
+*/
+
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+
+}

3762486cd6c1f9c5f56c1073e2e457085046cdeb
cbkidd <ckiddgodsea@gmail.com>
2020-06-12 01:05:39 +0000

    add file 7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - prints alphabet
+*
+* Return: 0
+*/
+
+int main(void)
+{
+
+int n;
+
+n = 123;
+
+while (n > 97)
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+
+return (0);
+}

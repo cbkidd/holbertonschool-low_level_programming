0513170499460d04b906d7dbdf899df2bd72c96e
cbkidd <ckiddgodsea@gmail.com>
2020-06-16 21:36:37 +0000

    create all main.c files and setup function for _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* _islower - returns 1 if c is lowercase
+*
+* Return: 0, or 1
+*/
+
+int _islower(int c)
+{
+	if (c > 64 && c < 91)
+	{
+	return (0);
+	}
+	else
+	{
+	return (1);
+	}
+}

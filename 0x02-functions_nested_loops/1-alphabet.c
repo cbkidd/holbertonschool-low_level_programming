b335b18988d1a1a87e1d6e4c996a0089f443a4dc
cbkidd <ckiddgodsea@gmail.com>
2020-06-16 20:19:51 +0000

    add files for function print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* print_alphabet - prints alphabet in lowercase
+*
+*
+*/
+
+void print_alphabet(void)
+{
+int i;
+
+for (i = 97; i < 123; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}

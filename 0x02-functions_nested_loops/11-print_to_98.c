387f1d7f070817d161e3973f9b07330491aa6d48
cbkidd <ckiddgodsea@gmail.com>
2020-06-17 23:13:57 +0000

    add files for print_to_98 function

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,24 +9,26 @@
 
 void print_to_98(int n)
 {
-	if (n > 98)
-	{
-	for (; n > 98; n--)
-	{
-	printf("%d, ", n);
-	}
-	}
-
 	if (n < 98)
 	{
-	for (; n < 98; n++)
+		for (n = n; n < 98; n++)
+		{
+			printf("%d", n);
+			printf(",");
+			printf(" ");
+		}
+	}
+	if (n > 98)
 	{
-	printf("%d, ", n);
+		for (n = n; n > 98; n--)
+	{
+			printf("%d", n);
+			printf(",");
+			printf(" ");
+		}
 	}
-	}
-
 	if (n == 98)
 	{
-	printf("%d", n);
+		printf("%d\n", n);
 	}
 }

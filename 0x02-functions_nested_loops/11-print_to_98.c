3897eb110f4007a794ca753818ebfee4ea25109b
cbkidd <ckiddgodsea@gmail.com>
2020-06-17 20:41:37 +0000

    add files for print_to_98 function

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* print_to_98 - makes n print to 98
+* @n: integer input
+*
+*/
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+	for (; n > 98; n--)
+	{
+	printf("%d, ", n);
+	}
+	}
+
+	if (n < 98)
+	{
+	for (; n < 98; n++)
+	{
+	printf("%d, ", n);
+	}
+	}
+}

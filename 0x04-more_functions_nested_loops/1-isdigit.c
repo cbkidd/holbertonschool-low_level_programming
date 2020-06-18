56d9dc3d1a984fcebf0f160d4bfde0b97b5a4b16
cbkidd <ckiddgodsea@gmail.com>
2020-06-18 20:10:24 +0000

    add files that complete _isdigit function

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* _isdigit - returns 1 if c is a digit
+* @c: ascii value tested
+* Return: 0, or 1
+*/
+
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+return (0);
+}

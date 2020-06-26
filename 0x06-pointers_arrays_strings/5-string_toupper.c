1c72bee406d67c6a67f2fe3b1bb3cffcd4d61509
cbkidd <ckiddgodsea@gmail.com>
2020-06-26 04:07:41 +0000

    add file for function string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,17 +9,16 @@ char *string_toupper(char *s)
 {
 int i;
 int j;
-
 	while (s[i])
 	{
-		i++
+	i++;
 	}
 
 	for (j = 0; j < i; j++)
 	{
-		if (s[i] >= 97 && s[i] <= 122)
+		if (s[j] >= 97 && s[j] <= 122)
 		{
-			s[i] -= 32;
+			s[j] -= 32;
 		}
 	}
 return (s);

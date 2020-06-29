84e0b6e87aca0542a660f1974ec9d32e5f2e4dff
cbkidd <ckiddgodsea@gmail.com>
2020-06-29 21:38:18 +0000

    add files for 2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* _strchr - locates a character in a string
+*
+* @s: string
+* @c: character to find
+*
+* Return: *s or 0
+*/
+
+char *_strchr(char *s, char c)
+{
+
+for (;; s++)
+	{
+if (*s == c)
+		{
+	return ((char *)s);
+		}
+	}
+return (0);
+}

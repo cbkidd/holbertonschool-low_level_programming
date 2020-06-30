572bce2f292caf0e301cead9d88ce9837135ba10
cbkidd <ckiddgodsea@gmail.com>
2020-06-30 23:26:09 +0000

    add files for 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,41 +1,29 @@
 #include "holberton.h"
 
 /**
+* strstr - finds sub string
 *
+* @haystack: main string
+* @needle: substring
 *
-*
-*
-*
-*
+* Return: 0
 */
 
 char *_strstr(char *haystack, char *needle)
 {
 
-int i;
-int j;
-int ye;
-
-i = 0;
-j = 0;
-
-while (haystack[i] != '\0')
+	for (; *haystack != '\0'; haystack++)
 	{
-	while (haystack[i] != needle[0] && haystack[i] != '\0')
-	{
-		i++;
-	}
-	if (haystack[i] == '\0')
-	{
-		return (-1);
-	}
-	ye = i;
-	
-	while (haystack[i] == needle[j] && haystack[i] != '\0' && needle[j] != '\0')
-	{
-	i++;
-	j++;
-	}
-	}
+		char *h = haystack;
+		char *n = needle;
 
-
+		while (*h == *n && (*n != '\0'))
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+			return (haystack);
+	}
+	return (0);
+}

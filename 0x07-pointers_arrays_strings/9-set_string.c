d79d631dcfdf9f1e80819e1f363dc019e20b7942
cbkidd <ckiddgodsea@gmail.com>
2020-06-30 04:51:36 +0000

    I think I did it?

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* set_string - sets double pointer to char
+* @s: string pointer
+* @to: char
+*
+* Return: null
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}

b1bcfea316cd132f0f7c983d26c8967b9dd0ee60
cbkidd <ckiddgodsea@gmail.com>
2020-07-02 03:46:27 +0000

    add dummy file

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* wildcmp - does thing
+* @s1: string
+* @s2: string dos
+* Return: 1 or 0
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+if (s1 == s2)
+{
+return (1);
+}
+return (0);
+}
--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
-*
-*
-*
+* is_palindrome - palindrome
+* @s: string
+* Return: s
 */
 
 int is_palindrome(char *s)

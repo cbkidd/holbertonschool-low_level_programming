15c969fd878ad8ae261775c995dddf52c19fd76f
cbkidd <ckiddgodsea@gmail.com>
2020-07-06 01:29:13 +0000

    add file for 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,19 +16,20 @@ int main(int argc, char **argv)
 int product;
 int var1;
 int var2;
+
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+
 var1 = atoi(argv[1]);
 var2 = atoi(argv[2]);
-product = (var1 * var2);
+product = (var1 *var2);
 
 if (argc == 3)
 {
 printf("%d\n", product);
-}
-
-if (argc < 2)
-{
-printf("Error\n");
-return (1);
 }
 
 return (0);

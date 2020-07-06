86652f224b29a95188eba7103ea23665b73e74b0
cbkidd <ckiddgodsea@gmail.com>
2020-07-06 00:46:32 +0000

    add file 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* main - prints each argument fed into main
+* @argc: string length
+* @argv: string
+*
+* Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+int i;
+i = 0;
+while (i < argc)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}

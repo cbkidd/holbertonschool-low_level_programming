85cd8c850cd7b7f6d078818a05a053a7b8951b1f
cbkidd <ckiddgodsea@gmail.com>
2020-07-08 03:14:30 +0000

    add advanced dummy

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* strtow - makes strings word
+* @str: string
+*
+*
+* Return: NULL
+*/
+
+char **strtow(char *str)
+{
+
+if (str == NULL)
+{
+return (NULL);
+}
+
+return (NULL);
+}

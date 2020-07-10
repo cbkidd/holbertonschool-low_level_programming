6e7d83fbf3c958eb87e7f42a9d3c7377b5be240a
cbkidd <ckiddgodsea@gmail.com>
2020-07-10 02:50:44 +0000

    add files for 2-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,76 @@
+#include "holberton.h"
+
+/**
+* _strlen - gets string length
+*
+* @s: string
+* Return: i
+*/
+
+int _strlen(char *s)
+{
+
+int i;
+
+if (s = NULL)
+	return (0);
+
+for (i = 0; s[i] != '\0'; i++)
+{
+continue;
+}
+return (i);
+}
+
+#include "holberton.h"
+
+/**
+* string_nconcat - concatenates 2 strings to n bytes
+* @s1: string 1
+* @s2: string 2
+* @n: number of bytes to allocate to concatenation
+*
+* Return: a or NULL
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *a;
+unsigned int i;
+unsigned int j;
+
+unsigned int len1 = _strlen(s1);
+unsigned int len2 = _strlen(s2);
+
+if (s1 == NULL)
+	s1 = "";
+
+if (s2 == NULL)
+	s2 = "";
+
+a = malloc(sizeof(char) * (s1 + n + 1));
+
+if (a == NULL)
+{
+	free(a);
+	return (NULL);
+}	
+
+for (i = 0; i < len1; i++)
+{
+	a[i] = s1[i];
+}
+for (j = 0; j < n; j++)
+{
+	if (j < len2)
+	{
+		a[i] = s2[j];
+	}
+	else
+	{
+		a[i] = '\0';
+	}
+}
+a[i] = '\0';
+return (a);
+}
---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+* _realloc - does stuff
+* @ptr: pointer
+* @old_size: old size
+* @new_size: new size
+*
+*
+*
+*
+*
+* Return: null
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+if (ptr != '\0')
+	ptr = 0;
+
+if (old_size != '\0')
+	old_size = 0;
+
+if (new_size != '\0')
+	new_size = 0;
+return (NULL);
+}

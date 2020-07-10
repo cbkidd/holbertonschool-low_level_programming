ae4991bb06ec5db3a468a2eaa07e5d5109c323db
cbkidd <ckiddgodsea@gmail.com>
2020-07-10 00:20:23 +0000

    add new dummy file

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* argstostr - ye
+* @ac: thing 1
+* @av: thing 2
+*
+*
+*
+*
+* Return: Null
+*/
+
+char *argstostr(int ac, char **av)
+{
+if (ac == 0 || av == 0)
+	return (NULL);
+
+printf("\n");
+
+return (NULL);
+}

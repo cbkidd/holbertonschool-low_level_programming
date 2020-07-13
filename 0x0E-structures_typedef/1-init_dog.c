42375402f988ba715932bc5d122312f22166a991
cbkidd <ckiddgodsea@gmail.com>
2020-07-13 23:40:37 +0000

    add files for 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+* init_dog - assigns each argument to d of struct dog
+* @d: points to struct dog initialization
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+*
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

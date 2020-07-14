5d6e7c6938f2dacc726128e1dd0c22d19b86acbc
cbkidd <ckiddgodsea@gmail.com>
2020-07-14 19:55:11 +0000

    fix 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,3 +1,48 @@
+#include "holberton.h"
+/**
+* _strdup - copies string into new allocation of mem
+*
+* @str: pointer to array
+*
+* Return: Null or New location address
+*
+*
+*
+*/
+
+char *_strdup(char *str)
+{
+
+char *p;
+unsigned int i = 0;
+
+if (str == 0)
+{
+	return (NULL);
+}
+
+while (str[i] != 0)
+{
+	i++;
+}
+
+i++;
+
+p = malloc(sizeof(char) * i);
+
+if (p == NULL)
+{
+	return (NULL);
+}
+
+for (i = 0; str[i] != 0; i++)
+{
+	p[i] = str[i];
+}
+p[i] = 0;
+return (p);
+}
+
 #include "dog.h"
 #include <stdlib.h>
 
@@ -16,8 +61,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 
 dog_t *newgoodboi;
-char *n = name;
-char *o = owner;
+char *n = _strdup(name);
+char *o = _strdup(owner);
 
 newgoodboi = malloc(sizeof(dog_t));
 if (newgoodboi == NULL)

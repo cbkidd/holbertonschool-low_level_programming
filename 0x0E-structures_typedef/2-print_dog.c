cd1ce24d75d9aa89070486dc8aabfc1a62a67137
cbkidd <ckiddgodsea@gmail.com>
2020-07-14 01:09:28 +0000

    add files for 3

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*
+*
+*
+*
+*
+*
+*
+*/
+
+void print_dog(struct dog *d)
+{
+
+if (d != NULL)
+{
+	printf("Name: %s\n", (*d).name);
+	printf("Age: %f\n", (*d).age);
+	printf("Owner: %s\n", (*d).owner);
+}
+if ((*d).name == NULL)
+	printf("Name: (nil)");
+
+if ((*d).age == 0)
+	printf("Age: (nil)");
+
+if ((*d).owner == NULL)
+	printf("Owner: (nil)");
+}
